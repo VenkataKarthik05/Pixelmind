03f7d35f5ded4a6e9dbed128470836f9266626db
VenkataKarthik <venkatakarthikvenna@gmail.com>
2026-05-07 18:14:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Services from "./components/Services";
 import Hero from "./Sections/Hero";
 import Results from "./components/Results";
 import Footer from "./components/Footer";
-import Aboutus from "./Pages/AboutUs";
+import Aboutus from "./Pages/Aboutus";
 import WhyChooseus from "./components/WhyChooseus";
 import SplashCurtain from "./components/loader";
 import WhatsAppButton from "./components/Whatsapp"; 
